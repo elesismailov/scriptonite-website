f0dc8f608fdbc69d83810ea0f28bde01fea1f426
Eles <eleswastaken@gmail.com>
2023-03-23 11:30:17 +0300

    Style albums section

--- components/SectionAlbums.js
+++ components/SectionAlbums.js
@@ -1,4 +1,6 @@
-
+import SpotifyIcon from "@/components/SpotifyIcon";
+import DeezerIcon from "@/components/DeezerIcon";
+import AppleMusicIcon from "@/components/AppleMusicIcon";
 
 export default function SectionAlbums() {
 
@@ -6,13 +8,68 @@ export default function SectionAlbums() {
         <section className="albums">
             <h2>Альбомы доступны на всех площадках</h2>
             <ul>
-            <li><img src="/images/album-1.png" width='250' height='250' alt="Album Preview." /></li>
-            <li><img src="/images/album-2.png" width='250' height='250' alt="Album Preview." /></li>
-            <li><img src="/images/album-3.png" width='250' height='250' alt="Album Preview." /></li>
-            <li><img src="/images/album-4.png" width='250' height='250' alt="Album Preview." /></li>
-            <li><img src="/images/album-5.png" width='250' height='250' alt="Album Preview." /></li>
-            <li><img src="/images/album-6.png" width='250' height='250' alt="Album Preview." /></li>
+                <li onMouseOver={handleOver} onMouseOut={handleOut}>
+                    <ul className="links">
+                        <li><a href=""><SpotifyIcon /></a></li>
+                        <li><a href=""><AppleMusicIcon /></a></li>
+                        <li><a href=""><DeezerIcon /></a></li>
+                        <li><a href=""><SpotifyIcon /></a></li>
+                    </ul>
+                    <img src="/images/album-1.png" width='250' height='250' alt="Album Preview." /></li>
+                <li onMouseOver={handleOver} onMouseOut={handleOut}>
+                    <ul className="links">
+                        <li><a href=""><SpotifyIcon /></a></li>
+                        <li><a href=""><AppleMusicIcon /></a></li>
+                        <li><a href=""><DeezerIcon /></a></li>
+                        <li><a href=""><SpotifyIcon /></a></li>
+                    </ul>
+                    <img src="/images/album-2.png" width='250' height='250' alt="Album Preview." /></li>
+                <li onMouseOver={handleOver} onMouseOut={handleOut}>
+                    <ul className="links">
+                        <li><a href=""><SpotifyIcon /></a></li>
+                        <li><a href=""><AppleMusicIcon /></a></li>
+                        <li><a href=""><DeezerIcon /></a></li>
+                        <li><a href=""><SpotifyIcon /></a></li>
+                    </ul>
+                    <img src="/images/album-3.png" width='250' height='250' alt="Album Preview." /></li>
+                <li onMouseOver={handleOver} onMouseOut={handleOut}>
+                    <ul className="links">
+                        <li><a href=""><SpotifyIcon /></a></li>
+                        <li><a href=""><AppleMusicIcon /></a></li>
+                        <li><a href=""><DeezerIcon /></a></li>
+                        <li><a href=""><SpotifyIcon /></a></li>
+                    </ul>
+                    <img src="/images/album-4.png" width='250' height='250' alt="Album Preview." /></li>
+                <li onMouseOver={handleOver} onMouseOut={handleOut}>
+                    <ul className="links">
+                        <li><a href=""><SpotifyIcon /></a></li>
+                        <li><a href=""><AppleMusicIcon /></a></li>
+                        <li><a href=""><DeezerIcon /></a></li>
+                        <li><a href=""><SpotifyIcon /></a></li>
+                    </ul>
+                    <img src="/images/album-5.png" width='250' height='250' alt="Album Preview." /></li>
+                <li onMouseOver={handleOver} onMouseOut={handleOut}>
+                    <ul className="links">
+                        <li><a href=""><SpotifyIcon /></a></li>
+                        <li><a href=""><AppleMusicIcon /></a></li>
+                        <li><a href=""><DeezerIcon /></a></li>
+                        <li><a href=""><SpotifyIcon /></a></li>
+                    </ul>
+                    <img src="/images/album-6.png" width='250' height='250' alt="Album Preview." /></li>
             </ul>
         </section>
     )
+}
+
+function handleOver(event) {
+    console.log('hovered')
+    console.log(event.target)
+    const album = event.target.parentElement;
+    album.classList.toggle('hovered')
+}
+
+function handleOut(event) {
+    console.log('left')
+    const album = event.target.parentElement;
+    album.classList.toggle('hovered')
 }
